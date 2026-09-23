2feb84af027cf53076cae0ac8a3e20ee0fab7a4b
Aleksander Kartuzov <aleksander.kartuzov23@gmail.com>
2026-09-23 10:58:57 +0300

    Job jumpscare

--- index.php
+++ index.php
@@ -1,43 +1,38 @@
 <?php
-class Box {
-    public $length;
-    public $width;
-    public $height;
 
-    public function volume() {
-        return $this->length * $this->width * $this->height;
+class Job {
+    public function task(Logger $logger) {
+        for($i=0; $i<=10; $i++) {
+            $logger = new ConsoleLogger();
+            $logger->log("Task $i completed!");
+        }
+    }
+} 
+
+class ConsoleLogger {
+    public function log($message) {
+        echo "$message\n";
     }
 }
 
-$box1= new Box();
-$box1->length = 5;
-$box1->width = 10;
-$box1->height = 2;
-var_dump($box1);
-echo "Volume: " . $box1->volume() . "\n\n";
-
-$box2= new Box();
-$box2->length = 15;
-$box2->width = 20;
-$box2->height = 3;
-var_dump($box2);
-echo "Volume: " . $box2->volume() . "\n\n";
-
-class MetalBox extends Box {
-    public $metalType;
-
-    public function volumeType($length, $width, $height, $metalType) {
-        $this->length = $length;
-        $this->width = $width;
-        $this->height = $height;
-        $this->metalType = $metalType;
-    }
-
-    public function getMetalType() {
-        return $this->metalType;
+class NothingLogger {
+    public function log($message) {
+        // 'Random kid'
     }
 }
 
-$metalBox1 = new MetalBox();
-$metalBox1->volumeType(10, 15, 5, "Aluminum");
-var_dump($metalBox1);
+interface Logger {
+    public function log($message);
+}
+
+class FileLogger implements Logger {
+    public function log($message) {
+        $file = fopen('log.txt', 'a');
+        fwrite($file, "$message\n");
+        fclose($file);
+    } 
+}
+
+$job = new Job();
+$logger = new FileLogger();
+$job->task($logger);
